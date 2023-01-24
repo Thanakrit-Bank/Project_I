1b8617e50d006d07e290ce1155f4246f723c8d16
NiponKamfoy <s6201012630061@email.kmutnb.ac.th>
2023-01-24 13:49:47 +0700

    untrack prepare_data folder

--- flask_api/prepare_data/convert_shp_geojson.py
+++
@@ -1,3 +0,0 @@
-import geopandas
-myshpfile = geopandas.read_file(r"C:\Users\s6201\Downloads\Data_Project\Country boundaries of Southeast Asia\data\commondata\data0\cntry_3m.shp")
-myshpfile.to_file('country.json', driver='GeoJSON')
--- flask_api/prepare_data/exercise.py
+++
@@ -1,8 +0,0 @@
-import os
-dir_list2 = os.listdir(r"F:\data_nc\data_project\ecearth\_SPI")
-temp = []
-for i in dir_list2:
-    name = i.split(".")[0].split("_")[-1]
-    temp.append(name)
-
-print(set(temp))
